229a725815d8520c12f41051edbfd9141011482b
Saleem Khan <saleemkanjla786@gmail.com>
2021-02-04 21:19:10 +0530

    todo complete

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
 from .forms import TaskForm
 from .models import Task
 # Create your views here.
@@ -11,6 +12,17 @@ class todoView(LoginRequiredMixin,View):
         ctx = dict({'form':form, 'task_list':task_list})
         return render(request,"todo/todo.html",context=ctx)
     def post(self,request):
+        if request.is_ajax():
+            print("RECEIVED SELECT AJAX POST REQUEST....")
+            id=request.POST['id']
+            deleteRequest = request.POST['delete']
+            task = Task.objects.get(pk=id)
+            if deleteRequest=='false':
+                task.finished = not task.finished
+                task.save()
+            else:
+                task.delete()
+            return JsonResponse({"success":True})
         form = TaskForm(request.POST)
         if not form.is_valid():
             return render(request,"todo/todo.html",{'form':form})
